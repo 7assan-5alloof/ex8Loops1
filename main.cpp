4a2ac6040efcb4fd1bf584d907d312973e68086b
7assan-5alloof <hassankhalloof20@gmail.com>
2021-03-30 15:12:18 +0400

    Write Program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop..." << endl;
+
+    while (true)
+    {
+        // Define needed variables
+        int i = 0;
+        int product = 1;
+        int sum = 0;
+
+        // Process 10 numbers
+        while (i < 10)
+        {
+            // Get needed info
+            int input;
+            cout << "Enter a number: ";
+            cin >> input;
+
+            // Add the given number to the sum and multiply the product by it
+            sum += input;
+            product *= input;
+
+            // Increment i to stop when 10 numbers are given
+            i++;
+        }
+
+        // Display sum and product
+        cout << "Sum: " << sum << endl << "Product: " << product << endl;
+    }
+
     return 0;
 }
